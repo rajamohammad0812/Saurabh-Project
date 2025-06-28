a1289d54f65d14b964b85fcf5945e0b6be420e33
meghnath mathur <38305667+meghnath1605@users.noreply.github.com>
2025-06-28 09:32:45 +0400

    Delete functions directory

--- functions/CreateGithubRepoActivity/run.ps1
+++
@@ -1,9 +0,0 @@
-param($name)
-
-$token = $env:GITHUB_TOKEN
-$body = @{ name = $name } | ConvertTo-Json
-
-Invoke-RestMethod -Uri "https://api.github.com/user/repos" `
-  -Method POST `
-  -Headers @{ Authorization = "token $token"; "User-Agent" = "AzureFunction" } `
-  -Body $body
--- functions/CreateKeyVaultActivity/run.ps1
+++
@@ -1,6 +0,0 @@
-param($name)
-
-$keyVaultRg = $env:KEYVAULT_RG
-$keyVaultLocation = $env:KEYVAULT_LOCATION
-
-az keyvault create --name $name --resource-group $keyVaultRg --location $keyVaultLocation | ConvertFrom-Json
