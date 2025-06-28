6878ed53bedcc4231594d261f3288991e00cf56f
meghnath mathur <38305667+meghnath1605@users.noreply.github.com>
2025-06-28 09:32:56 +0400

    Delete infra directory

--- infra/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "function_app_name" {
-  value = azurerm_function_app.durable_function.name
-}
--- infra/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "function_name" {}
-variable "resource_group_name" {}
-variable "location" {}
